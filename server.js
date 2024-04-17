80b2d919cbd72bc3d450dfcff00e4b207bb2b112
citlallivillarreal <citlallivillarreal@outlook.com>
2024-04-17 16:38:03 -0700

    Added DB configurations

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import cors from 'cors';
+import knex from 'knex';
+import 'dotenv/config';
+
+knex({
+    client: process.env.DB_CLIENT,
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+  });
 
 const app = express();
 
