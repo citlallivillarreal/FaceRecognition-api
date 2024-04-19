b13d0d71ff38e1c546fe7e85dc9bafdb23c5b889
citlallivillarreal <citlallivillarreal@outlook.com>
2024-04-19 15:05:40 -0700

    Added database integration to all routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import knex from 'knex';
 import 'dotenv/config';
 
-knex({
+const db = knex({
     client: process.env.DB_CLIENT,
     connection: {
       host: process.env.DB_HOST,
@@ -18,86 +18,89 @@ knex({
 const app = express();
 
 app.use(express.json());
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            enteries: 0,
-            joined: new Date()
-        }, 
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            enteries: 0,
-            joined: new Date()
-        }
-    ]
-}
+
+
 app.use(cors());
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('success');
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-        } else {
-            res.status(400).json('error logging in');
-        }
-    res.json('signin');
+    db.select('email', 'hash').from('login')
+        .where('email' , '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))   
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
     const { email, password, name } = req.body;
     const saltRounds = 10;
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
-            console.log(hash);
-        });
-    });
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        enteries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length - 1]);
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    // bcrypt.genSalt(saltRounds, function(err, salt) {
+    //     bcrypt.hash(password, salt, function(err, hash) {
+    //         console.log(hash);
+    //     });
+    // });
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true; 
-            return res.json(user);
+    db.select('*').from('users').where({id})
+        .then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('Not found')
         }
     })
-    if (!found) {
-        res.status(404).json('no such user');
-    }
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true; 
-            user.enteries ++
-            return res.json(user.enteries);
-        }
-    })
-    if (!found) {
-        res.status(404).json('no such user');
-    }
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
 
 })
 // bcrypt.genSalt(saltRounds, function(err, salt) {
