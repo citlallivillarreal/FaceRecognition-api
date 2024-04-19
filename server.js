c2943a5acbaa517ae93585438756df5c7136dd13
citlallivillarreal <citlallivillarreal@outlook.com>
2024-04-19 16:21:04 -0700

    Added controllers for all endpoints

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import bcrypt from 'bcrypt';
 import cors from 'cors';
 import knex from 'knex';
 import 'dotenv/config';
+import { handleRegister } from './controllers/register.js';
+import { handleSignin } from './controllers/signin.js';
+import { handleProfileGet } from './controllers/profile.js';
+import { handleImage } from './controllers/image.js';
 
 const db = knex({
     client: process.env.DB_CLIENT,
@@ -25,89 +29,14 @@ app.get('/', (req, res) => {
     res.send('success');
 })
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email' , '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))   
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin',  handleSignin(db, bcrypt))
 
-app.post('/register', (req, res) => {
-    const { email, password, name } = req.body;
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(password, salt);
-    // bcrypt.genSalt(saltRounds, function(err, salt) {
-    //     bcrypt.hash(password, salt, function(err, hash) {
-    //         console.log(hash);
-    //     });
-    // });
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0]);
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-        if (user.length) {
-            res.json(user[0])
-        } else {
-            res.status(400).json('Not found')
-        }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
+app.get('/profile/:id', (req, res) => {handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0].entries);
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
+app.put('/image', (req, res) => {handleImage(req, res, db)})
 
-})
-// bcrypt.genSalt(saltRounds, function(err, salt) {
-//     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-//         // Store hash in your password DB.
-//     });
-// });
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
